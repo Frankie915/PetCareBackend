07728794cc3921c968937b8a76d26454753e55a6
Frankie915 <frankmm083@gmail.com>
2023-06-13 01:27:51 -0400

    try and catch implemented. submit no longer crashes

--- app/src/main/java/com/zybooks/petcare/PetRegistration.java
+++ app/src/main/java/com/zybooks/petcare/PetRegistration.java
@@ -1,2 +1,26 @@
-package com.zybooks.petcare;public class PetRegistration {
+package com.zybooks.petcare;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PetRegistration {
+
+    public boolean regex(String input, String re){
+        Pattern pt = Pattern.compile(re);
+        Matcher mt = pt.matcher(input);
+        return mt.matches();
+    }
+
+    public boolean checkEmail(String email){
+        System.out.println(email);
+        return regex(email, "^[A-Za-z0-9._%+-]{3,}@[A-Za-z0-9.-]+\\.(?:edu|co|com|gal)$");
+    }
+
+    public boolean checkCode(int accessCode, int confirmCode) {
+        if(accessCode != confirmCode)
+            return false;
+        else
+            return true;
+    }
 }
+
