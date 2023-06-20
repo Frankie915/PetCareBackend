cb295e38608cfae24b29727bde0f3db8305ca5e0
Frankie915 <frankmm083@gmail.com>
2023-06-19 21:09:52 -0400

    linear.xml added

--- app/src/main/java/com/zybooks/petcare/MainActivity.java
+++ app/src/main/java/com/zybooks/petcare/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constraint);
+        setContentView(R.layout.linear);
 
         PetReg = new PetRegistration();
 
