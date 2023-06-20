1593f15feff52211b6ea8718da0bf542a1e61d62
Frankie915 <frankmm083@gmail.com>
2023-06-20 13:34:38 -0400

    grid.xml added

--- app/src/main/java/com/zybooks/petcare/MainActivity.java
+++ app/src/main/java/com/zybooks/petcare/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear);
+        setContentView(R.layout.relative);
 
         PetReg = new PetRegistration();
 
