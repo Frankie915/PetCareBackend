c8ce750c02b414c4fde61b8e3d1c43d95a534021
Frankie915 <frankmm083@gmail.com>
2023-07-20 17:06:51 -0400

    added PetDatabase and PetRepository

--- app/src/main/java/com/zybooks/petcare/repo/PetDatabase.java
+++ app/src/main/java/com/zybooks/petcare/repo/PetDatabase.java
@@ -5,5 +5,7 @@ import androidx.room.RoomDatabase;
 import com.zybooks.petcare.Model.PetInfo;
 
 @Database(entities = {PetInfo.class}, version = 2)
-public class PetDatabase {
+public abstract class PetDatabase extends RoomDatabase {
+
+public abstract PetInfoDao petInfoDao();
 }
